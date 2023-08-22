b374362fd68905b123bc00d191437d260fabe700
Shane Kelly <shaneryankelly@gmail.com>
2023-08-22 07:17:05 -0400

    line parser
    working

--- Assets/Scripts/GlobalController.cs
+++ Assets/Scripts/GlobalController.cs
@@ -47,13 +47,47 @@ public class GlobalController : MonoBehaviour
         {
             //find the npc and assign dialogues.
             NPC currentNPC = GameObject.Find(scenes.scenes[0].NPCs[i].name).GetComponent<NPC>();
-            Debug.Log(dialogues.dialogues[0].states[0].lines[0]);
-            //This is a total unmitigated disaster, fix it oh my god please.
-            /*for (int j = 0; j < dialogues.dialogues[currentNPC.NPCIndex].dialogues[currentState].lines.Count; j++){
-                Debug.Log(dialogues.dialogues[currentNPC.NPCIndex].dialogues[currentState]);
-                currentNPC.script.Add(dialogues.dialogues[currentNPC.NPCIndex].dialogues[currentState].lines[j]);
-            }*/
+            //Debug.Log(dialogues.dialogues[0].states[0].lines[0]);
+            for (int j = 0; j < dialogues.dialogues[currentNPC.NPCIndex].states[currentState].lines.Count; j++){
+                string currentLine = dialogues.dialogues[currentNPC.NPCIndex].states[currentState].lines[j];
+                Debug.Log(currentLine);
+                string parsedLine = parseDialogue(currentLine);
+                Debug.Log(parsedLine);
+                currentNPC.script.Add(parsedLine);
+            }
         }
+    }
+
+    private string parseDialogue(string rawString){
+        // find embedded var and insert value
+        bool parsingVar = false;
+        string currentVar = "";
+        int varStart = 0;
+        int varEnd = 0;
+        char[] stringArray = rawString.ToCharArray();
+        Debug.Log(rawString);
+        for (int i = 0; i < rawString.Length; i++){
+            if (stringArray[i] == '{'){
+                varStart = i;
+                parsingVar = true;
+                Debug.Log("parsing variable name");
+            }
+            else if (stringArray[i] == '}'){
+                varEnd = i+1;
+                Debug.Log(rawString.Substring(varStart, varEnd-varStart));
+                rawString = rawString.Replace(rawString.Substring(varStart, varEnd-varStart), PlayerPrefs.GetString(currentVar));
+                currentVar = "";
+                varStart = 0;
+                varEnd = 0;
+                Debug.Log("var end");
+                parsingVar = false;
+            }
+            else if (parsingVar){
+                currentVar += stringArray[i];
+            }
+        }
+        Debug.Log(rawString);
+        return rawString;
     }
 
     public string ReturnString(){
